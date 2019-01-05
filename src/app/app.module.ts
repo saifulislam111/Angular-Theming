c36953c3ac245537217e3cdae4a3a5f2a832dd70
Saiful <saiful.islam@televersant.com>
2019-01-06 03:40:36 +0600

    themes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule, MatListModule
+} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
